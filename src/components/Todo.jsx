080a35f18d0e492d8b54d98c54786bd1e4e2c0af
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-04-17 01:31:51 -0700

    adding all the components

---
+++ src/components/Todo.jsx
@@ -0,0 +1,34 @@
+export default function Todo({ todo, setTodos, todos, filteredTodos }) {
+  const deleteHandler = () => {
+    setTodos(
+      todos.filter((el) => {
+        return el.id !== todo.id;
+      })
+    );
+  };
+
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return { ...item, completed: !item.completed };
+        }
+        return item;
+      })
+    );
+  };
+
+  return (
+    <div className="todo">
+      <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+        {todo.text}
+      </li>
+      <button onClick={completeHandler} className="complete-btn">
+        <i className="fas fa-check"></i>
+      </button>
+      <button onClick={deleteHandler} className="trash-btn">
+        <i className="fas fa-trash"></i>
+      </button>
+    </div>
+  );
+}
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,21 @@
+import Todo from "./Todo";
+
+export default function TodoList({ todos, setTodos, filteredTodos }) {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        {filteredTodos.map((todo) => {
+          return (
+            <Todo
+              todo={todo}
+              key={todo.id}
+              setTodos={setTodos}
+              todos={todos}
+              filteredTodos={filteredTodos}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
